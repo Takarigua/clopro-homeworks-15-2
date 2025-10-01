185986122d3f2740df489dcec67fe737a74a56e1
Takarigua <49216259+Takarigua@users.noreply.github.com>
2025-10-01 21:46:08 +1100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,31 @@
+variable "yandex_cloud_id" {
+  default = "b1ggqfo0dsargh78crnv"
+}
+
+variable "yandex_folder_id" {
+  default = "b1goq41hp3i45ccfhhjk"
+}
+
+variable "a-zone" {
+  default = "ru-central1-a"
+}
+
+variable "bucket_name" {
+  default = "takarigua-netology"
+}
+
+variable "image_lamp" {
+  default = "fd827b91d99psvq5fjit"
+}
+
+variable "access_key" {
+  default = "YCAJEJ7jrcvPKsNUpsK1DvVd9"
+}
+
+variable "secret_key" {
+  default = "***"
+}
+
+variable "service_account_id" {
+  default = "ajeb61cu3d6mur9s9cg3"
+}
