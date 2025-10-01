0b252773ee98afbaa23319d9644977c1b8667a34
Takarigua <49216259+Takarigua@users.noreply.github.com>
2025-10-01 21:44:36 +1100

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = ">= 0.87.0"
+    }
+  }
+  required_version = ">= 1.13.0"
+}
+
+provider "yandex" {
+  # Используем уже созданный файл key.json в каталоге модуля
+  service_account_key_file = "${path.module}/key.json"
+  cloud_id  = var.yandex_cloud_id
+  folder_id = var.yandex_folder_id
+  zone      = var.a-zone
+}
